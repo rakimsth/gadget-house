8b5f9f322a028ecbd56f9862dbb7ed0cf56dd449
Raktim Shrestha <raktim@rumsan.com>
2023-10-03 09:27:15 +0545

    routing and bootstrap setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Cart from "./pages/Cart";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "*",
+    element: <Error />,
+  },
+]);
+
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
