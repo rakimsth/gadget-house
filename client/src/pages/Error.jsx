3d1b6454d5dd78fb1f4d467eaec6bf6b34208a65
Raktim Shrestha <raktim@rumsan.com>
2023-10-03 09:30:12 +0545

    error page cleanup

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -5,10 +5,11 @@ const Error = () => {
         <div className="text-center">
           <h1 className="display-1 fw-bold">404</h1>
           <p className="fs-3">
-            {" "}
-            <span className="text-danger">Opps!</span> Page not found.
+            <span className="text-danger">Oops!</span> Page not found.
           </p>
-          <p className="lead">The page you’re looking for doesn’t exist.</p>
+          <p className="lead">
+            The page you&apos;re looking for doesn&apos;t exist.
+          </p>
           <a href="/" className="btn btn-primary">
             Go Home
           </a>
