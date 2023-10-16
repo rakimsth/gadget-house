7d28bea8c1c4646406a86de0c04846f64531afc2
Raktim Shrestha <raktim@rumsan.com>
2023-10-16 09:23:36 +0545

    seed product update upto 10

--- server/database/seed.js
+++ server/database/seed.js
@@ -42,7 +42,7 @@ var setup = {
 
     console.log("Creating Products");
     await productController.create({
-      name: "Black Tshirt",
+      name: "Black One Tshirt",
       brand: "Zara",
       price: 1500,
       quantity: 10,
@@ -56,7 +56,7 @@ var setup = {
       ],
     });
     await productController.create({
-      name: "Blue Jeans",
+      name: "Blue Two Jeans",
       brand: "Zara",
       price: 2500,
       quantity: 5,
@@ -77,6 +77,96 @@ var setup = {
       category: cat2?._id,
       images: [
         "https://thumbs.dreamstime.com/b/blue-jeans-isolated-white-34440719.jpg",
+      ],
+    });
+    await productController.create({
+      name: "Black Three Tshirt",
+      brand: "Zara",
+      price: 1500,
+      quantity: 10,
+      description: "Best Tshirt in the market",
+      sku: "t-002",
+      category: cat1?._id,
+      images: [
+        "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+        "https://www.jiomart.com/images/product/original/rvowvf0akl/eyebogler-men-s-polo-neck-regular-fit-half-sleeves-colorblocked-teal-t-shirt-product-images-rvowvf0akl-0-202211051905.jpg?im=Resize=(500,630)",
+        "https://thehouseofrare.com/cdn/shop/products/IMG_0053_5c650849-9d9d-4cc3-8863-6a23778cd9a0.jpg?v=1675170808",
+      ],
+    });
+    await productController.create({
+      name: "Blue Three Jeans",
+      brand: "Zara",
+      price: 2500,
+      quantity: 5,
+      description: "Best Jeans in the market",
+      sku: "J-003",
+      category: cat2?._id,
+      images: [
+        "https://cdn.pixelbin.io/v2/black-bread-289bfa/iix9pB/wrkr/t.resize(h:600,w:510)/data/gas/21072022/410316769_A222_1.jpg",
+      ],
+    });
+    await productController.create({
+      name: "High Skinny Tight Two Jeans",
+      brand: "Tommys",
+      price: 2500,
+      quantity: 0,
+      description: "Best Skinny Fit Jeans in the market",
+      sku: "J-004",
+      category: cat2?._id,
+      images: [
+        "https://thumbs.dreamstime.com/b/blue-jeans-isolated-white-34440719.jpg",
+      ],
+    });
+    await productController.create({
+      name: "Black Four Tshirt",
+      brand: "Zara",
+      price: 1500,
+      quantity: 10,
+      description: "Best Tshirt in the market",
+      sku: "t-004",
+      category: cat1?._id,
+      images: [
+        "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+        "https://www.jiomart.com/images/product/original/rvowvf0akl/eyebogler-men-s-polo-neck-regular-fit-half-sleeves-colorblocked-teal-t-shirt-product-images-rvowvf0akl-0-202211051905.jpg?im=Resize=(500,630)",
+        "https://thehouseofrare.com/cdn/shop/products/IMG_0053_5c650849-9d9d-4cc3-8863-6a23778cd9a0.jpg?v=1675170808",
+      ],
+    });
+    await productController.create({
+      name: "Blue Four Jeans",
+      brand: "Zara",
+      price: 2500,
+      quantity: 5,
+      description: "Best Jeans in the market",
+      sku: "J-004",
+      category: cat2?._id,
+      images: [
+        "https://cdn.pixelbin.io/v2/black-bread-289bfa/iix9pB/wrkr/t.resize(h:600,w:510)/data/gas/21072022/410316769_A222_1.jpg",
+      ],
+    });
+    await productController.create({
+      name: "High Skinny Tight Three Jeans",
+      brand: "Tommys",
+      price: 2500,
+      quantity: 0,
+      description: "Best Skinny Fit Jeans in the market",
+      sku: "J-005",
+      category: cat2?._id,
+      images: [
+        "https://thumbs.dreamstime.com/b/blue-jeans-isolated-white-34440719.jpg",
+      ],
+    });
+    await productController.create({
+      name: "Black Five Tshirt",
+      brand: "Zara",
+      price: 1500,
+      quantity: 10,
+      description: "Best Tshirt in the market",
+      sku: "t-005",
+      category: cat1?._id,
+      images: [
+        "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+        "https://www.jiomart.com/images/product/original/rvowvf0akl/eyebogler-men-s-polo-neck-regular-fit-half-sleeves-colorblocked-teal-t-shirt-product-images-rvowvf0akl-0-202211051905.jpg?im=Resize=(500,630)",
+        "https://thehouseofrare.com/cdn/shop/products/IMG_0053_5c650849-9d9d-4cc3-8863-6a23778cd9a0.jpg?v=1675170808",
       ],
     });
     console.log("---------DONE----------");
